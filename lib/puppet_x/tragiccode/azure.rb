d2910c3231617035a3b2a234ffdb4966b1b5e534
stuartj <stuart.jales@weareact.com>
2021-01-12 14:54:51 +0000

    [AUTO-2797] If there is no nextLink, set it to an empty string as nil has no empty? method.

--- lib/puppet_x/tragiccode/azure.rb
+++ lib/puppet_x/tragiccode/azure.rb
@@ -41,7 +41,7 @@ module TragicCode
       raise res.body unless res.is_a?(Net::HTTPSuccess)
       secrets_res = JSON.parse(res.body)['value']
       logger.info("TragicCode::Azure::get_secrets - Initial secrets found: #{secrets_res}")
-      next_page = JSON.parse(res.body)['nextLink']
+      next_page = JSON.parse(res.body).key?('nextLink') ? JSON.parse(res.body)['nextLink'] : ""
       while not next_page.empty?
         logger.info("TragicCode::Azure::get_secrets - Getting next page: #{next_page}")
         uri = URI(next_page)
@@ -53,7 +53,7 @@ module TragicCode
         raise res.body unless res.is_a?(Net::HTTPSuccess)
         logger.info("TragicCode::Azure::get_secrets - Adding secrets: #{JSON.parse(res.body)['value']}")
         secrets_res = secrets_res + JSON.parse(res.body)['value']
-        next_page = JSON.parse(res.body)['nextLink']
+        next_page = JSON.parse(res.body).key?('nextLink') ? JSON.parse(res.body)['nextLink'] : ""
       end
       logger.info("TragicCode::Azure::get_secrets - Found secrets: #{secrets_res}")
       return secrets_res
